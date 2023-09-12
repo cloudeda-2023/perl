0860dd068986a683600c5ddd5b6f67c4fbcf1b2b
cloudeda-2023 <144735659+cloudeda-2023@users.noreply.github.com>
2023-09-11 19:49:58 -0700

    Update simple_def_compare.pl

--- simple_def_compare.pl
+++ simple_def_compare.pl
@@ -1,5 +1,5 @@
 # This program is dedicated to the public domain under the CC0 license.
-# Cloud EDA 2023
+# Created by Cloud EDA 2023
 
 use strict ;
 use warnings;
@@ -8,16 +8,23 @@ use warnings;
 my $GOLDEN_DEF = $ARGV[0];
 my $REFERENCE_DEF = $ARGV[1];
 
+my $args = "@ARGV";
+
+my $error= 0;
+if ( $args =~ /-error_only/  ) { $error=1; }
+
 # variable to know the state of parsing
 my $parse = 0;
-open(FH1,"<$GOLDEN_DEF")||die("cannot open file $GOLDEN_FILE");
+my @_data;
+my $_tmp;
+my %save_data;
+open(FH1,"<$GOLDEN_DEF")||die("cannot open file $GOLDEN_DEF");
 while(<FH1>) {
   # go into parsing state 
   if ( /^\s*COMPONENTS/ ) {    $parse = 1;  } 
   # exit the parsing state , in this case we are not parsing multiple sections of DEF . So we can stop reading from file 
-  if ( $parse == 1  && /^\s*END COMPONENTS/) {    $parse = 0;    last;   }
-
-  if ( $parse == 1 ) {
+  elsif ( $parse == 1  && /^\s*END COMPONENTS/) {    $parse = 0;    last;   }
+  elsif ( $parse == 1 ) {
     # based on the DEF format , capture necessary attributes .
     # this is not generic code and needs to change if the format changes
 
@@ -30,15 +37,14 @@ while(<FH1>) {
   
 }
 close FH1;
-# done loading one DEF into memory now , process second file ;
 
+# done loading one DEF into memory now , process second file ;
 open(FH1,"<$REFERENCE_DEF")||die("cannot open file $REFERENCE_DEF");
 while(<FH1>) {
   # same as above 
   if ( /^COMPONENTS/ ) {    $parse = 1;  } 
-  if ( $parse == 1  && /^\s*END COMPONENTS/) {    $parse = 0;    last;   }
-
-  if ( $parse == 1 ) {
+  elsif ( $parse == 1  && /^\s*END COMPONENTS/) {    $parse = 0;    last;   }
+  elsif ( $parse == 1 ) {
     @_data = split('\s+',$_);
     # save current value into a temporary variable for comparing 
     $_tmp = "$_data[6] $_data[7] $_data[9]";
@@ -50,9 +56,9 @@ while(<FH1>) {
       # current instance is present in the keys list of %save_data
       # now compare if the values are the same , i.e did the location or orientation change ?
       if ( $save_data{$_data[1]} ne $_tmp ) {
-        print "ERROR : $_data[1] location or orientation is different between $REFERENCE_DEF and $GOLDEN_DEF \n";
+        print "ERROR : $_data[1] location or orientation is different between $REFERENCE_DEF($_tmp) and $GOLDEN_DEF(".$save_data{$_data[1]}.")\n";
       } else {
-        print "INFO : no change in $_data[1] location or orientation \n";
+        if ( $error == 0 ) { print "INFO : no change in $_data[1] location or orientation \n"; }
       }
 
       # unload current instance from memory 
@@ -66,7 +72,7 @@ close FH1;
 # this is the reason why we unloaded data after processing 
 # are there any instances in GOLDEN which are not present in REF ?
 # any key which is not deleted in REF file parising loop is the answer 
-foreach $_inst ( keys %save_data ) {
+foreach my $_inst ( keys %save_data ) {
   print "ERROR : $_inst present in $GOLDEN_DEF but not in $REFERENCE_DEF \n";
 }
 

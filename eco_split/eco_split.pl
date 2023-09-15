26d41b20a1346aff194df7a433982dfadee39629
cloudeda-2023 <144735659+cloudeda-2023@users.noreply.github.com>
2023-09-14 18:25:04 -0700

    Update eco_split.pl

--- eco_split/eco_split.pl
+++ eco_split/eco_split.pl
@@ -5,36 +5,40 @@
 ##                                            Cloud EDA 2023
 ##########################################################################################################################
 
-use strict ;
-use warnings;
 
 # instance_name nickname ref_name
 my $str="ddr_inst DDR DRR_TOP
 ADC_0 ADC0 ADC_TOP
-DAC_0 DAC0 DAC_TOP;
+DAC_0 DAC0 DAC_TOP";
 
-
+my @sp;
+my %lookup;
+my %fh_lookup;
+my $_tmp;
 my $cnt=0;
 foreach my $entry (split("\n",$str)) {
 	@sp = split('\s+',$entry);
 	$lookup{$sp[0]} = $sp[1];
   	$fh_lookup{$sp[0]} = "FH$cnt";
- 	open("FH$cnt","<$sp[1].eco")||die("cannot open file $sp[1].eco\n");
+	$_tmp = "FH$cnt";
+ 	open($_tmp,">$sp[1].eco")||die("cannot open file $sp[1].eco\n");
   	$cnt++;
 }
 
 
 my $save_str = "";
-open(my TOP,">./TOP.eco");
+open(TOP,">./TOP.eco");
 
-open(FH1,"<$ARGV[0]");
-while(<FH1>) {
+open(IN,"<$ARGV[0]");
+while(<IN>) {
 	if (/^\s*current_instance\s*$/) {
 		# check and split
   		my $binned = 0;
   		foreach my $inst ( keys %lookup ) {
     			if ( $save_str =~ /$inst/ ) {
-       				print $fh_lookup{$inst} $save_str;
+				$_tmp = $fh_lookup{$inst};
+				$save_str =~ s/ $inst\/?/ /g;	
+       				print $_tmp $save_str;
 	   			$binned = 1;
 			}
 		}
@@ -48,9 +52,11 @@ while(<FH1>) {
 		
 }
 
-close FH1;
+close IN;
 close TOP;
 foreach my $inst ( keys %lookup ) {
-	close $fh_lookup{$inst};
+	$_tmp =  $fh_lookup{$inst};
+	close $_tmp;
 }
+
 

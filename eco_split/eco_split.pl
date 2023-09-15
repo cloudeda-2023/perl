10f75859079632ff0d8d811ab9b1ca2f2d8eb6cf
cloudeda-2023 <144735659+cloudeda-2023@users.noreply.github.com>
2023-09-14 20:46:30 -0500

    Update eco_split.pl

--- eco_split/eco_split.pl
+++ eco_split/eco_split.pl
@@ -6,16 +6,19 @@
 ##########################################################################################################################
 
 
-# instance_name nickname ref_name
+# instance_name nickname ref_name , need to add for each block hierarchy . even for reuse list all of the reuse instances
 my $str="ddr_inst DDR DRR_TOP
 ADC_0 ADC0 ADC_TOP
 DAC_0 DAC0 DAC_TOP";
 
+# variable declarations 
 my @sp;
 my %lookup;
 my %fh_lookup;
 my $_tmp;
 my $cnt=0;
+
+# this loop creates unique file handles per instance based on $str varaible
 foreach my $entry (split("\n",$str)) {
 	@sp = split('\s+',$entry);
 	$lookup{$sp[0]} = $sp[1];
@@ -27,13 +30,16 @@ foreach my $entry (split("\n",$str)) {
 
 
 my $save_str = "";
+# any eco which doesn't go into block is will directed into TOP level eco file
 open(TOP,">./TOP.eco");
 
+# open the input EDA tool generated ECO file
 open(IN,"<$ARGV[0]");
 while(<IN>) {
 	if (/^\s*current_instance\s*$/) {
 		# check and split
   		my $binned = 0;
+    		# check if the current ECO is for any of the listed blocks , if yes write into the corresponding file
   		foreach my $inst ( keys %lookup ) {
     			if ( $save_str =~ /$inst/ ) {
 				$_tmp = $fh_lookup{$inst};
@@ -42,6 +48,7 @@ while(<IN>) {
 	   			$binned = 1;
 			}
 		}
+  		# if the current eco is not binned to any block , then bin into top level eco file
 		if ( $binned == 0 )  {
   			print TOP $save_str;
 		}
@@ -52,6 +59,7 @@ while(<IN>) {
 		
 }
 
+# cleanup/close file handles
 close IN;
 close TOP;
 foreach my $inst ( keys %lookup ) {
